e094812c172ff0c188927795dfe894355ad4b58c
Luca2409 <luca.oppold@outlook.de>
2026-08-29 20:32:01 +0200

    adjust links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://lucaschwarz.io',
-  integrations: [sitemap()],
+    site: 'https://lucaschwarz.io',
+    path: '/',
+    integrations: [sitemap()],
 });
